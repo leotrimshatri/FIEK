cd27de698f8cfa283fff2485642226279a4d56b6
Leotrim Shatri <77514315+leotrimshatri@users.noreply.github.com>
2022-03-03 14:40:55 +0100

    Create definimi_klasave.cpp

---
+++ LIGJERATA/JAVA 1/definimi_klasave.cpp
@@ -0,0 +1,23 @@
+/* 
+Në C++, strukturat dhe klasat kanë një dallim : derisa qasja e
+nënkuptuar (ang. default access) te strukturat është publike (ang.
+public), te klasat kjo qasje e nënkuptuar është private (ang. private).
+*/ 
+
+// Programi class1
+#include <iostream>
+using namespace std;
+class person
+{
+public:
+char emri[15];
+char qyteti[10];
+int viti;
+};
+int main()
+{
+}
+
+/*
+
+  
