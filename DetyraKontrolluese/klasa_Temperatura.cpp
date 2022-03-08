36c5333b7e47530bfb20542665b84559c5194f55
Leotrim Shatri <77514315+leotrimshatri@users.noreply.github.com>
2022-03-08 16:42:19 +0100

    Update klasa_Temperatura.cpp

--- DetyraKontrolluese/klasa_Temperatura.cpp
+++ DetyraKontrolluese/klasa_Temperatura.cpp
@@ -26,75 +26,79 @@ b. [12 pikë] Në pjesën publike, klasa Temperatura duhet të përmbaj funksion
 c. [2 pikë] Funksionin main, ku krijohet një objekt nga klasa temparatura dhe thirrën të gjitha
 funksionet e klasës për t’i shtypur rezultatet e llogaritura.
 */
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 using namespace std;
-class temperatura {
 
+class temperatura
+{
 private:
-      list<int> L;
+    list<int> L;
+
 public:
-      void Mbushja() {
-              for (int i = 1; i <= 7; i++)
-              {
-                     int n;
-                     cout << "Cakto nje vlere n per anetarin i=" << i <<"; n=";
-                     cin >> n;
-                     if (n >= 5 && n <= 20)
-                     {
-                             L.push_back(n);
-                      else
-                      {
-                             cout << "\nGabiml Vlera n duhet te jete mes 5 dhe 20 (Scanc=20). \n";
-                             i--;
-                             continue;
-                      }
-                     }
-                cout << endl; 
-              }
-     
-       
-        void Shtypja() {
-         for (auto i = L.begin(); i != L.end(); i++)
-                {
-                cout << *i << " ";
-                }
-                cout << endl;
-                }
-       
-        int Numero() {
-         int k = 0;
-          for (auto i = L.begin(); i != L.end(); i++)
-          {
-              if (*i >= 10)
-          {
-           k++;
-          }
-          }
-          return k;
-          }
-
-        void Zgjerimi() {
-          int s = 0, p = 1;
-          for (auto i = L.begin(); i != L.end(); i++)
-          {
-            s *= (*i);
+    void Mbushja()
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            int n;
+            cout << "Cakto nje vlere n per anetarin i=" << i << "; n=";
+            cin >> n;
+            if (n >= 5 && n <= 20)
+            {
+                L.push_back(n);
+            }
+            else
+            {
+                cout << "\nGabim! Vlera n duhet te jete mes 5 dhe 20 (5<=n<=20).\n";
+                i--;
+                continue;
+            }
+        }
+        cout << endl;
+    }
+    void Shtypja()
+    {
+        for (auto i = L.begin(); i != L.end(); i++)
+        {
+            cout << *i << " ";
+        }
+        cout << endl;
+    }
+    int Numero()
+    {
+        int k = 0;
+        for (auto i = L.begin(); i != L.end(); i++)
+        {
+            if (*i >= 10)
+            {
+                k++;
+            }
+        }
+        return k;
+    }
+    void Zgjerimi()
+    {
+        int s = 0, p = 1;
+        for (auto i = L.begin(); i != L.end(); i++)
+        {
+            s += (*i);
             p *= (*i);
-          }
-          L.push_front(s); L.push_front(p);
-            
+        }
+        L.push_front(s);
+        L.push_back(p);
+    }
+};
 
-           
-
-          void main() {
-              temperatura t;
-              t.Mbushja();
-              
-              cout << "Lista: ";
-              t.Shtypja();
-                  cout << "Numri i anetareve dyshifror: " << t.Numero() << endl;;
-              t.Zgjerimi();
-                  cout << "Lista pas zgjerimit: ";
-              t.Shtypja();
-                system("Pause");
+int main()
+{
+    temperatura t;
+    t.Mbushja();
+    cout << "Lista: ";
+    t.Shtypja();
+    cout << "Numri i anetareve dyshifror: " << t.Numero() << endl;
+    ;
+    t.Zgjerimi();
+    cout << "Lista pas zgjerimit: ";
+    t.Shtypja();
+    system("Pause");
 }
