59f0232da852b3adfe8a66a48c3a46a96e8261c5
Leotrim Shatri <77514315+leotrimshatri@users.noreply.github.com>
2022-03-21 11:06:21 +0100

    Create NULLpoint.cpp

---
+++ USHTRIME/JAVA 4/NULLpoint.cpp
@@ -0,0 +1,18 @@
+
+/* Detyra 9
+	NULL pointeri
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int* p = nullptr;
+	int* p1 = NULL;
+
+	cout << p << endl;
+	cout << p1 << endl;
+	
+	return 0;
+}
