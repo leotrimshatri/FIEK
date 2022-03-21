667f15591d0b30215b7d1ddb096447a105307c01
Leotrim Shatri <77514315+leotrimshatri@users.noreply.github.com>
2022-03-21 11:07:12 +0100

    Create references.cpp

---
+++ USHTRIME/JAVA 4/references.cpp
@@ -0,0 +1,26 @@
+/* Detyra 11
+	Deklarimi i referencave
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 3;
+	int& b = a;
+
+	cout << "Adresa e a: " << &a << endl;
+	cout << "Vlera e a: " << a << endl;
+	cout << "Adresa e b: " << &b << endl;
+	cout << "Vlera e b: " << b << endl;
+
+	b = 6;
+
+	cout << "Adresa e a: " << &a << endl;
+	cout << "Vlera e a: " << a << endl;
+	cout << "Adresa e b: " << &b << endl;
+	cout << "Vlera e b: " << b << endl;
+	
+	return 0;
+}
